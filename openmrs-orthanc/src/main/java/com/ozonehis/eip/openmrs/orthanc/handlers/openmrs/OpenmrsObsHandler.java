9343fddde90d6b9a284bd9c6bc514d43bef65f2d
corneliouzbett <kipchubett@gmail.com>
2025-06-23 13:39:12 +0300

    OZ-942: `OpenmrsObsHandler` to include authorization headers for OpenMRS API requests

--- openmrs-orthanc/src/main/java/com/ozonehis/eip/openmrs/orthanc/handlers/openmrs/OpenmrsObsHandler.java
+++ openmrs-orthanc/src/main/java/com/ozonehis/eip/openmrs/orthanc/handlers/openmrs/OpenmrsObsHandler.java
@@ -10,6 +10,7 @@ package com.ozonehis.eip.openmrs.orthanc.handlers.openmrs;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ozonehis.eip.openmrs.orthanc.Constants;
+import com.ozonehis.eip.openmrs.orthanc.config.OpenmrsConfig;
 import com.ozonehis.eip.openmrs.orthanc.models.obs.Attachment;
 import com.ozonehis.eip.openmrs.orthanc.models.obs.AttachmentObs;
 import java.util.HashMap;
@@ -17,17 +18,24 @@ import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.ProducerTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class OpenmrsObsHandler {
 
+    @Autowired
+    private OpenmrsConfig openmrsConfig;
+
     public List<Attachment> getObsByPatientUUIDAndConceptUUID(
             ProducerTemplate producerTemplate, String patientID, String conceptID) throws JsonProcessingException {
         Map<String, Object> headers = new HashMap<>();
         headers.put(Constants.HEADER_OPENMRS_PATIENT_UUID, patientID);
         headers.put(Constants.HEADER_OPENMRS_OBS_CONCEPT_UUID, conceptID);
+        headers.put(Constants.CAMEL_HTTP_METHOD, Constants.GET);
+        headers.put(Constants.CONTENT_TYPE, Constants.APPLICATION_JSON);
+        headers.put(Constants.AUTHORIZATION, openmrsConfig.authHeader());
         String response = producerTemplate.requestBodyAndHeaders(
                 "direct:orthanc-get-openmrs-obs-route", null, headers, String.class);
         AttachmentObs results = new ObjectMapper().readValue(response, AttachmentObs.class);
